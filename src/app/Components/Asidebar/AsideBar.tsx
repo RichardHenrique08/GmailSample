3def260c5a5cbbbbc580e9be34a2050418d031a7
Richard Henrique <105549763+RichardHenrique08@users.noreply.github.com>
2024-05-27 21:19:25 +0000

    Moficações Aside

--- src/app/Components/Asidebar/AsideBar.tsx
+++ src/app/Components/Asidebar/AsideBar.tsx
@@ -12,30 +12,30 @@ const AsideBar = () => {
     <>
       <div className="wAside ">
         <div className="bg-blue-200 w-1/2 ml-3 mb-3 mt-5 py-3 px-2 rounded-md flex justify-around">
-          <a href="#" className="flex items-center gap-2">
+          <a href="#" className="flex items-center gap-4">
             <LuPencil />
             Escrever
           </a>
         </div>
         <div>
           <ul className="pl-8 flex flex-col gap-1">
-            <li className="flex items-center gap-1">
+            <li className="flex items-center gap-4">
               <FaInbox />
               Caixa de entrada
             </li>
-            <li className="flex items-center gap-1">
+            <li className="flex items-center gap-4">
               <FaRegStar />
               Com estrela
             </li>
-            <li className="flex items-center gap-1">
+            <li className="flex items-center gap-4">
               <IoTimeOutline />
               Adiados
             </li>
-            <li className="flex items-center gap-1">
+            <li className="flex items-center gap-4">
               <CiPaperplane />
               Enviados
             </li>
-            <li className="flex items-center gap-1">
+            <li className="flex items-center gap-4">
               <RiFilePaper2Line />
               Rascunho
             </li>
